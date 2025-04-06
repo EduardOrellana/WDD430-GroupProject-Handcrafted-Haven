7e34b56f8c802ea23bda85408fbe3208a39c1d0a
EduardOrellana <ore23006@byui.edu>
2025-04-06 00:29:46 -0600

    Fixing Functions

--- app/ui/home-category-list.tsx
+++ app/ui/home-category-list.tsx
@@ -1,32 +1,6 @@
 import styles from "../page.module.css"
 import Link from "next/link"
-
-const categoriesDommyData = [
-    {
-        id: 1,
-        title: "electronics",
-        description: "This is a great category 1",
-        imageUrl: "https://placehold.co/150",
-    },
-    {
-        id: 2,
-        title: "clothing",
-        description: "This is a great category 2",
-        imageUrl: "https://placehold.co/150",
-    },
-    {
-        id: 3,
-        title: "technology",
-        description: "This is a great category 3",
-        imageUrl: "https://placehold.co/150",
-    },
-    {
-        id: 4,
-        title: "games",
-        description: "This is a great category 4",
-        imageUrl: "https://placehold.co/150",
-    }
-]
+import { productsObject } from "@/app/lib/temporalData";
 
 export default function CategoryList() {
 
@@ -34,10 +8,10 @@ export default function CategoryList() {
         <div className={styles.categoryList}>
             <h2>Categories' List</h2>
             <ul> 
-            {categoriesDommyData.map((category, index) => (
+            {productsObject.map((category, index) => (
                 <li className="card" key={index}>
-                    <Link href={`/users/products/${category.title}`}>
-                        {category.title}
+                    <Link href={`/users/products/${category.category}`}>
+                        {category.category}
                     </Link>
                 </li>
             ))}
