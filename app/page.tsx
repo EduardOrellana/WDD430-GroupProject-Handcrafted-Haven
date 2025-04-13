61d8044fe1ca797fd9cf0bdbbcc9313cb3ee63a0
EduardOrellana <ore23006@byui.edu>
2025-04-12 20:36:04 -0600

    Almost Finish Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,22 @@ import SellerList from './ui/home-seller-list';
 import CategoryList from './ui/home-category-list';
 import Search from './ui/search';
 import styles from './page.module.css';
+import InfoHome from './ui/info-home';
 
 export default function Home() {
   return (
     <>
-      <h1>Welcome to Heavenly Haven Art Store</h1>
-      <Search />
-      <div className={styles.home}>
-        <SellerList />
-        <CategoryList />
+      <div className = {styles.homePage}>
+        <div className = {styles.searchContainer}>
+          <Search />
+        </div>
+        <div className = {styles.mainContainer}>
+          <InfoHome />
+        </div>
+        <div className={styles.linksContainer}>
+          <SellerList />
+          <CategoryList />
+        </div>
       </div>
     </>
   );
